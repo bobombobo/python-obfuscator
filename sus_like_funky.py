3395e0a9e2f490640754461a2b80235fcb28e27f
bobombobo <49346538+bobombobo@users.noreply.github.com>
2021-05-16 13:43:21 -0500

    Added top obfuscation text :)
    Made it harder for humans to read and find the actual code :) still a beta release and still dooky butt variable names lol.

--- sus_like_funky.py
+++ sus_like_funky.py
@@ -5,7 +5,7 @@ import string
 
 # list of one and zero to chose from for the random.choice
 # new variable name type
-var_type = 3
+var_type = 1
 if var_type == 1:
     whathtenuts = [str(1),str(0)]
 elif var_type == 2:
@@ -17,7 +17,7 @@ else:
 
 
 code=('''
-print("haha lol XD :flushed:")
+print("idk anymore dude")
 ''')
 
 # V byte code
@@ -70,24 +70,20 @@ randobj5 = dudewhatthenuts+(''.join(random.choice(whathtenuts) for i in range(12
 randobj6 = dudewhatthenuts+(''.join(random.choice(whathtenuts) for i in range(12)))
 randobj7 = dudewhatthenuts+(''.join(random.choice(whathtenuts) for i in range(12)))
 
-
-final_script = ("""
-watermark = ("Wow this is all it takes to make an 'obfuscator'... sad!")
-
-{hahahahabase} = eval(''.join(chr(int("00111000"[i*8:i*8+8],2)) for i in range(len("00111000")//8)))
-{randobj1} = eval(''.join(chr(int("01100101011110000110010101100011"[i*8:i*8+8],2)) for i in range(len("01100101011110000110010101100011")//8)))
-{randobj2} = eval(''.join(chr(int("00100111001001110010111001101010011011110110100101101110"[i*8:i*8+8],2)) for i in range(len("00100111001001110010111001101010011011110110100101101110")//8)))
-{randobj3} = eval(''.join(chr(int("011000110110100001110010"[i*8:i*8+8],2)) for i in range(len("011000110110100001110010")//8)))
-{randobj4} = eval(''.join(chr(int("011010010110111001110100"[i*8:i*8+8],2)) for i in range(len("011010010110111001110100")//8)))
-{randobj5} = eval(''.join(chr(int("0111001001100001011011100110011101100101"[i*8:i*8+8],2)) for i in range(len("0111001001100001011011100110011101100101")//8)))
-{randobj6} = eval(''.join(chr(int("011011000110010101101110"[i*8:i*8+8],2)) for i in range(len("011011000110010101101110")//8)))
-{randobj7} = eval(''.join(chr(int("00110010"[i*8:i*8+8],2)) for i in range(len("00110010")//8)))
-
+top_text_code = ("""watermark = ("Wow this is all it takes to make an 'obfuscator'... sad!");{hahahahabase} = eval(''.join(chr(int("00111000"[i*8:i*8+8],2)) for i in range(len("00111000")//8)));{randobj1} = eval(''.join(chr(int("01100101011110000110010101100011"[i*8:i*8+8],2)) for i in range(len("01100101011110000110010101100011")//8)));{randobj2} = eval(''.join(chr(int("00100111001001110010111001101010011011110110100101101110"[i*8:i*8+8],2)) for i in range(len("00100111001001110010111001101010011011110110100101101110")//8)));{randobj3} = eval(''.join(chr(int("011000110110100001110010"[i*8:i*8+8],2)) for i in range(len("011000110110100001110010")//8)));{randobj4} = eval(''.join(chr(int("011010010110111001110100"[i*8:i*8+8],2)) for i in range(len("011010010110111001110100")//8)));{randobj5} = eval(''.join(chr(int("0111001001100001011011100110011101100101"[i*8:i*8+8],2)) for i in range(len("0111001001100001011011100110011101100101")//8)));{randobj6} = eval(''.join(chr(int("011011000110010101101110"[i*8:i*8+8],2)) for i in range(len("011011000110010101101110")//8)));{randobj7} = eval(''.join(chr(int("00110010"[i*8:i*8+8],2)) for i in range(len("00110010")//8)));""").format(
+res=res,
+hahahahabase=hahahahabase, 
+farted=farted, 
+randobj1=randobj1, 
+randobj2=randobj2,
+randobj3=randobj3,
+randobj4=randobj4,
+randobj5=randobj5,
+randobj6=randobj6,
+randobj7=randobj7)
 
 
-
-{randobj1}({randobj2}({randobj3}({randobj4}(("{res}")[{farted}*{hahahahabase}:{farted}*{hahahahabase}+{hahahahabase}],{randobj7})) for {farted} in {randobj5}({randobj6}(("{res}"))//{hahahahabase})))
-""").format(
+final_script = ("""{randobj1}({randobj2}({randobj3}({randobj4}(("{res}")[{farted}*{hahahahabase}:{farted}*{hahahahabase}+{hahahahabase}],{randobj7})) for {farted} in {randobj5}({randobj6}(("{res}"))//{hahahahabase})))""").format(
 res=res,
 hahahahabase=hahahahabase, 
 farted=farted, 
@@ -104,38 +100,55 @@ randobj7=randobj7)
 #res = ''.join(chr(int(res[i*8:i*8+8],2)) for i in range(len(res)//8))
 
 
-
 #_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 #JSON obfuscation? idk man im bored
-
-import json
 #dud what the fuck am i doing
 
-esketit = {'balls': 'hi_lol', 'dick':{'cock':{'socks': 'fart', 'legodick': 'theexec_code', 'whendidiask':'print("hewwo")'}}}
-
-#fomrat is gonna be like:
-#    gen_layer = {'randomtext': 50/50 to either gen another layer or input text}
-whathtenuts = [1,0]
+#whathtenuts = [1,0]
 
 letters = string.ascii_lowercase + string.ascii_uppercase
-joemama = ( ''.join(random.choice(letters) for i in range(10)) )
+joemama = dudewhatthenuts+(''.join(random.choice(whathtenuts) for i in range(12)))
 
-def gen_layer(ammount):
-    ammount = ammount
-    layer=[]
-    layers_p_text = []
-    #decide = random.choice(whathtenuts)
-    for x in range(ammount):
-        random_text = ( ''.join(random.choice(letters) for i in range(10)) )
-        theform = ("'random_text': {").replace("random_text", random_text)
-        layers_p_text.append(random_text)
-        layer.append(theform)
-    code = ()
-    some_text_ig = ("{" + ''.join(layer) + 'exec("""penis_cum""")' +('}' * (ammount+1)))
-    print(some_text_ig.replace("penis_cum", final_script))
+
+ammount = 5
+layer=[]
+layers_p_text = []
+#decide = random.choice(whathtenuts)
+for x in range(ammount):
+    random_text = dudewhatthenuts+(''.join(random.choice(whathtenuts) for i in range(20)))
+    theform = ("'random_text': {").replace("random_text", random_text)
+    layers_p_text.append(random_text)
+    layer.append(theform)
+
+
+back_at_it = []
+back_p_text = []
+for x in range(ammount):
+    random_text = dudewhatthenuts+(''.join(random.choice(whathtenuts) for i in range(20)))
+    theform = ("'random_text': {").replace("random_text", random_text)
+    back_p_text.append(random_text)
+    back_at_it.append(theform)
     
+    
+some_text_ig = ("{" + ''.join(layer) + 'penis_cum' + ('}' * (ammount+1)))
 
-why_dis_no_work = 10
-gen_layer(why_dis_no_work)
+#just using random names now cuz i don't want to name anymore
+for x in range(3):
+    urwpu13948h5 = []
+    asd908ugunwqing = []
+    toup_text_ammount = 12 #12 line on top of the actual cod text
+    for x in range(toup_text_ammount):
+        random_text = dudewhatthenuts+(''.join(random.choice(whathtenuts) for i in range(200)))
+        theform = ("'random_text': {").replace("random_text", random_text)
+        urwpu13948h5.append(random_text)
+        asd908ugunwqing.append(theform)
+    
+    okayLesGo = ("{" + ''.join(asd908ugunwqing) + ('}' * (toup_text_ammount+1)))
+    print(okayLesGo)
 
-#dude what
+
+
+
+dickfuck = (some_text_ig.replace("penis_cum", final_script))
+print(top_text_code)
+print(dickfuck)
