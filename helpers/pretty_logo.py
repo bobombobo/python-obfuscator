973e6c469749e9ffda74afbe04358bbe1e6208b7
boboMbobo <49346538+bobombobo@users.noreply.github.com>
2022-01-27 09:57:54 -0600

    Version check update

--- helpers/pretty_logo.py
+++ helpers/pretty_logo.py
@@ -1,6 +1,23 @@
 #Honestly i could have made this myself but im to lazy
 #pulled from: https://github.com/KasRoudra/k-fuscator/blob/main/kf.py
 #purely for the color not even for the code loool
+import requests
+import os
+
+server_version = requests.get("https://raw.githubusercontent.com/bobombobo/python-obfuscator/main/version.txt").text
+#local_version = 
+path_parent = os.path.dirname(os. getcwd())
+os.chdir(path_parent)
+with open((os.getcwd()+"/main.py"), "r") as file:
+  content = file.readlines()
+  for line in content:
+    if server_version in line:
+      version_data = ("up-to-date")
+      file.close()
+      break
+  
+  version_data = ("outdated")
+
 black="\033[0;30m"
 red="\033[0;31m"
 green="\033[0;32m"
@@ -11,6 +28,19 @@ cyan="\033[0;36m"
 white="\033[0;37m"
 
 def pretty_logo_func(status):
+  server_version = requests.get("https://raw.githubusercontent.com/bobombobo/python-obfuscator/main/version.txt").text
+  #local_version = 
+  #path_parent = os.path.dirname(os. getcwd())
+  #os.chdir(path_parent)
+  with open("main.py", "r") as file:
+    content = file.readlines()
+    for line in content:
+      if server_version in line:
+        version_data = ("up-to-date")
+        file.close()
+        break
+      else:
+        version_data = ("outdated")
   text=("""
 """+purple+"""██╗░░██╗██████╗░██╗░░░░░██╗░░░░░░█████╗░
 """+blue+  """██║░░██║╚════██╗██║░░░░░██║░░░░░██╔══██╗
@@ -23,8 +53,18 @@ def pretty_logo_func(status):
 
   elif status==("done"):
     text=text+(purple+"\nMad with "+red+"♡"+purple+"  by [" +red+"boboMbobo"+purple+" | "+blue+"https://github.com/bobombobo/"+purple+"]"+cyan+"\nDiscord: "+blue+"https://discord.gg/vJxRgaX2wz"+white+"\n")
+
+
+
+    if str(version_data)==("up-to-date"):
+      text = text + (cyan +"Version: " + server_version)
+    elif version_data==("outdated"):
+      text = text+(red +"OLD VERSION! Please conider downloading the latest version\nfor secuirty updates!"+ blue + "\nLatest build " + server_version)
+    else:
+      text = text+("version check failed")
+
   return(text)
 
 
 if __name__=="__main__":
-  pretty_logo_func()
+  print(pretty_logo_func("done"))
